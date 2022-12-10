fd1696c155b63b03f5d706456883de0138dc7b7a
pavinane <pavimega77@gmail.com>
2022-12-11 00:54:01 +0530

    add packages and write own schema

---
+++ server/SampleData.js
@@ -0,0 +1,77 @@
+// Project
+
+const projects =[
+    {
+        id:"1",
+        clientId:'1',
+        name:"eCommerce Website",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        status:"In progress"
+    },
+    {
+        id:"2",
+        clientId:'2',
+        name:"Dating app",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        status:"In progress"
+    },
+    {
+        id:"3",
+        clientId:'3',
+        name:"SEO Project",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        status:"In progress"
+    },
+    {
+        id:"4",
+        clientId:'4',
+        name:"Design Prototype",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        status:"In progress"
+    },
+    {
+        id:"5",
+        clientId:'5',
+        name:"Action Website",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        status:"In progress"
+    },
+    
+]
+
+// clients
+
+const clients = [
+    {
+        id:'1',
+        name:"Tony Strak",
+        email:"tony@gamil.com",
+        phone:'343-524-4554'
+    },
+    {
+        id:'2',
+        name:"Bruce",
+        email:"bruce@gamil.com",
+        phone:'152-524-4551'
+    },
+    {
+        id:'3',
+        name:"Natasha",
+        email:"natasha@gamil.com",
+        phone:'977-154-4554'
+    },
+    {
+        id:'4',
+        name:"Thor",
+        email:"thor@gamil.com",
+        phone:'646-541-1143'
+    },
+    {
+        id:'5',
+        name:"Captain America",
+        email:"ca@gamil.com",
+        phone:'515-235-8516'
+    },
+]
+
+module.exports = { projects, clients};
---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv').config();
+const colors  = require('colors');
+const connectDB = require('./config/db');
+const {graphqlHTTP} = require('express-graphql');
+const port = process.env.PORT || 5000;
+const schema =  require('./schema/schema')
+const app = express();
+
+connectDB();
+
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql:process.env.NODE_ENV === 'development'
+}))
+
+app.listen(port,console.log(`server running on ${port}`));
